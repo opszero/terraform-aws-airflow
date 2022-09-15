68e06e40f1070026b99f81e330867680d1be5779
Abhi Yerra <abhi@berkeley.edu>
2022-09-15 13:25:35 -0700

    Initial

---
+++ main.tf
@@ -0,0 +1,40 @@
+resource "aws_mwaa_environment" "example" {
+  name = var.name
+
+#   network_configuration {
+#     security_group_ids = [aws_security_group.example.id]
+#     subnet_ids         = aws_subnet.private[*].id
+#   }
+
+#   source_bucket_arn = aws_s3_bucket.example.arn
+
+#   dag_s3_path        = "dags/"
+#   execution_role_arn = aws_iam_role.example.arn
+
+#   logging_configuration {
+#     dag_processing_logs {
+#       enabled   = true
+#       log_level = "DEBUG"
+#     }
+
+#     scheduler_logs {
+#       enabled   = true
+#       log_level = "INFO"
+#     }
+
+#     task_logs {
+#       enabled   = true
+#       log_level = "WARNING"
+#     }
+
+#     webserver_logs {
+#       enabled   = true
+#       log_level = "ERROR"
+#     }
+
+#     worker_logs {
+#       enabled   = true
+#       log_level = "CRITICAL"
+#     }
+#   }
+}
