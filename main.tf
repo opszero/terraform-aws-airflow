7c0e12d3b4ac4ca8a2bede5b6b7f9b71f5117522
Abhi Yerra <abhi@berkeley.edu>
2022-09-19 10:49:55 -0700

    Fix

--- main.tf
+++ main.tf
@@ -223,7 +223,7 @@ data "aws_iam_policy_document" "base" {
     resources = [
       "arn:aws:kms:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:key/*"
     ]
-    conditions = {
+    condition {
       test = "StringLike"
       variable = "kms:ViaService"
       values = [
